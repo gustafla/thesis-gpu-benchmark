77a76ced949f17c35e8305877b8233713f51dcbe
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2026-05-18 10:16:47 +0300

    build.zig: Inject patched SDL library

--- build.zig
+++ build.zig
@@ -5,12 +5,29 @@ pub fn build(b: *std.Build) void {
     // Initialize options and dependency
     const options = engine.Options.init(b);
     const engine_dep = b.dependency("mehustin2", options);
-    const patched_sdl_dep = b.dependency("sdl", .{
-        .target = options.target,
-        .optimize = options.optimize,
-    });
-    _ = patched_sdl_dep;
     const config = @import("src/config.zon");
+
+    // Inject patched SDL3
+    if (!options.system_sdl) {
+        const patched_sdl_dep = b.dependency("sdl", .{
+            .target = options.target,
+            .optimize = options.optimize,
+            .preferred_linkage = .static,
+            .strip = options.optimize != .Debug,
+            .sanitize_c = .off,
+        });
+        const patched_sdl_lib = patched_sdl_dep.artifact("SDL3");
+        for (engine_dep.module("engine").link_objects.items) |*object| {
+            switch (object.*) {
+                .other_step => |ostep| {
+                    if (std.mem.eql(u8, ostep.name, "SDL3")) {
+                        object.* = .{ .other_step = patched_sdl_lib };
+                    }
+                },
+                else => {},
+            }
+        }
+    }
 
     // Create script module
     const script_mod = b.createModule(.{
