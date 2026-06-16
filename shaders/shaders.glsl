79281f12a4cc8e9f7a0f121b5f0f9142520aebcf
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2026-06-16 16:14:05 +0300

    Add dense reference convolution filter

--- shaders/shaders.glsl
+++ shaders/shaders.glsl
@@ -82,6 +82,13 @@ void main() {
 }
 #endif // FRAGMENT and POST
 
+#if defined(FRAGMENT) && defined(CLEAR)
+layout(location = 0) out vec4 out_color;
+void main() {
+    out_color = vec4(vec3(0.0), 1.0);
+}
+#endif // FRAGMENT and CLEAR
+
 #if defined(FRAGMENT) && defined(POINT_IMAGE)
 in vec4 gl_FragCoord;
 
